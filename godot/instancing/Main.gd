79b3b546194fc9ec7d5c26f38a473b1e828ae18e
Dmitry Ponyatov <dponyatov@gmail.com>
2024-02-17 22:02:48 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- godot/instancing/Main.gd
+++ godot/instancing/Main.gd
@@ -5,5 +5,6 @@ export (PackedScene) var Ball
 func _input(event):
 	if event.is_action_pressed("click"):
 		var new_ball = Ball.instance()
-		new_ball.position = get_viewport().get_mouse_position()
+		var ms = get_viewport().get_mouse_position()
+		new_ball.position = ms
 		add_child(new_ball)
